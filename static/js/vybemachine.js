0f60f1f3b7495e2deb81f7437859723e484dd5a4
phantamanta44 <evandalong@gmail.com>
2015-04-28 13:13:28 -0500

    Added small startup animation

--- static/js/vybemachine.js
+++ static/js/vybemachine.js
@@ -1,3 +1,19 @@
-SC.initialize({
-    client_id: "b45b1aa10f1ac2941910a7f0d10f8e28"
+$(function() {
+
+    var init = function() {
+        var initDiv = $(document.getElementById('init'));
+        SC.initialize({client_id: "b45b1aa10f1ac2941910a7f0d10f8e28"});
+        setTimeout(function() {
+            initDiv.css('top', '-110%');
+            initDiv.css('color', '#1b1b1b');
+            $(document.body).css('background-color', '#eee');
+            setTimeout(function() {
+                initDiv.remove();
+            }, 1200);
+        }, 2718);
+        
+    };
+    
+    init();
+    
 });
