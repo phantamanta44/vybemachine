8642de45c0295ef48577b6262f25238597830b67
Evan Geng <evandalong@gmail.com>
2015-04-28 13:17:56 -0500

    Update CLIENT_ID

--- static/js/vybemachine.js
+++ static/js/vybemachine.js
@@ -2,7 +2,7 @@ $(function() {
 
     var init = function() {
         var initDiv = $(document.getElementById('init'));
-        SC.initialize({client_id: "b45b1aa10f1ac2941910a7f0d10f8e28"});
+        SC.initialize({client_id: "19a6c5e98aef00b45ab6d1ebdf3ca361"});
         setTimeout(function() {
             initDiv.css('top', '-110%');
             initDiv.css('color', '#1b1b1b');
