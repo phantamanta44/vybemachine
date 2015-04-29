4c677c39411ca640bed60e1f83b40568d8129320
Phanta <evandalong@gmail.com>
2015-04-28 19:21:08 -0500

    Styling changes

--- static/js/vybemachine.js
+++ static/js/vybemachine.js
@@ -4,8 +4,9 @@ $(function() {
         var initDiv = $(document.getElementById('init'));
         SC.initialize({client_id: "19a6c5e98aef00b45ab6d1ebdf3ca361"});
         setTimeout(function() {
-            initDiv.css('top', '-110%');
+            initDiv.css('top', '-84%');
             initDiv.css('color', '#1b1b1b');
+            $(document.getElementById('initimg')).css('-webkit-filter', 'invert(8%)');
             $(document.body).css('background-color', '#eee');
             setTimeout(function() {
                 initDiv.remove();
