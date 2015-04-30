556ff2e197f44cab7df0b124780729d56785119f
unknown <geng.evan.18@W-WHS-068750.iccsd.k12.ia.us>
2015-04-30 12:30:48 -0500

    JS implementation

--- static/js/vybemachine.js
+++ static/js/vybemachine.js
@@ -1,30 +1,67 @@
 $(function() {
-
-    var init = function() {
-        var initDiv = $(document.getElementById('init'));
-        var mainDiv = $(document.getElementById('main'));
+    
+    var currentSound;
+    var QUERY_VAL = ["trap", "edm", "dubstep", "glitchhop", "dance", "trance", ""];
+    
+    var initDiv = $(document.getElementById('init'));
+    var mainDiv = $(document.getElementById('main'));
+    var artworkDiv = $(document.getElementById('albumart'));
+    var artworkImg = $(document.getElementById('albumartfg'));
+    
+    var preInit = function() {
         soundManager.setup({url: 'static/swf/', onready: function() {
             soundManager.createSound({id: 'initSound', url: 'static/ogg/init.ogg', autoLoad: true, autoPlay: true, volume: 100});
+            SC.initialize({client_id: "19a6c5e98aef00b45ab6d1ebdf3ca361"});
+            setTimeout(init, 4800);
         }});
-        SC.initialize({client_id: "19a6c5e98aef00b45ab6d1ebdf3ca361"});
-        setTimeout(function() {
-            initDiv.css('top', '-84%');
-            initDiv.css('color', '#222');
-            $(document.getElementById('initimg')).css('-webkit-filter', 'invert(8%)');
-            $(document.body).css('background-color', '#eee');
-            setTimeout(function() {
-                initDiv.remove();
-                mainDiv.show();
-                mainDiv.fadeTo(1200, 1.0, postInit());
-            }, 1200);
-        }, 4850);
-        
     };
     
-    var postInit = function() {
-        
+    var init = function() {
+        initDiv.css('top', '-84%');
+        initDiv.css('color', '#222');
+        $(document.getElementById('initimg')).css('-webkit-filter', 'invert(8%)');
+        $(document.body).css('background-color', '#eee');
+        setTimeout(function() {
+            initDiv.remove();
+            mainDiv.show();
+            mainDiv.fadeTo(1200, 1.0, postInit());
+        }, 1200);
     }
     
-    init();
+    var postInit = function() {
+        while (true) {
+            if (currentSound == undefined || currentSound.playState == 0) {
+                randTrack();
+            }
+        }
+    }
+    
+    var randTrack = function() {
+        SC.get("/tracks", {limit: 1, tags: "edm", limit: 512, q: randSel(QUERY_VAL)}, function(tracks) {
+            var t = randInt(0, 512);
+            if (tracks[t].artwork_url != null) {
+                setArtwork(tracks[t].artwork_url);
+            }
+            currentSound = SC.stream("/tracks/" + tracks[t].id, function(sound) {
+                sound.play();
+            });
+            while (currentSound == undefined || currentSound.playState == 0) { }
+        });
+    }
+    
+    var setArtwork = function(artUrl) {
+        artworkDiv.css('background-image', "url(" + artUrl + ")");
+        artworkImg.attr('src', artUrl);
+    }
+    
+    var randInt = function(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+    
+    var randSel = function(list) {
+            return list[Math.floor(Math.random() * list.length)]
+    }
+    
+    preInit();
     
 });
